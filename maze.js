9fdd49aa162e4899da617bb6a629b98bf4936422
mossponsol <mossenialponsol@gmail.com>
2017-10-23 15:17:09 -0500

    Exercise 5 Complete

--- maze.js
+++ maze.js
@@ -2,17 +2,21 @@ window.onload = function(){
 	var boundaries = document.getElementsByClassName("boundary");
 	var end = document.getElementById("end");
 	var start = document.getElementById("start");
+	var status = document.getElementById("status");
+	var maze= document.getElementById("maze");
  	//Exercise 2
  	var gameOver = function() {
  		for (var i = 0; i < boundaries.length; i++) {
  			boundaries[i].className += " youlose";
  		}
+ 		status.innerText = "You LOSE!"; //Exercise 5
  	}
 
  	var restart = function(){
  		for (var i = 0; i < boundaries.length; i++) {
  			boundaries[i].className = "boundary";
  		}
+ 		status.innerText = "Good Luck"; //Exericse 5
  	}
  	for (var i = 0; i < boundaries.length; i++) {
 			boundaries[i].onmouseover = function(){
@@ -23,7 +27,7 @@ window.onload = function(){
 	//Exercise 3
 	end.onmouseover = function(){
 		if (!(boundaries[0].classList.contains("youlose"))){
-			alert("YOU WIN!");
+			status.innerText = "You WIN!"; //Exercise 5
 		}
 	}
 
@@ -31,5 +35,11 @@ window.onload = function(){
 	start.onmouseover = function(){
 		restart();
 	}
+
+	//Exercise 6
+	maze.onmouseleave = function(){
+		gameOver();
+	}
+
 }
 
