fd4109f979a24178f6562c98c9bfc337efe399ed
mossponsol <mossenialponsol@gmail.com>
2017-10-23 15:02:58 -0500

    Exercise 4 Completed

--- maze.js
+++ maze.js
@@ -1,10 +1,17 @@
 window.onload = function(){
 	var boundaries = document.getElementsByClassName("boundary");
 	var end = document.getElementById("end");
+	var start = document.getElementById("start");
  	//Exercise 2
  	var gameOver = function() {
  		for (var i = 0; i < boundaries.length; i++) {
  			boundaries[i].className += " youlose";
+ 		}
+ 	}
+
+ 	var restart = function(){
+ 		for (var i = 0; i < boundaries.length; i++) {
+ 			boundaries[i].className = "boundary";
  		}
  	}
  	for (var i = 0; i < boundaries.length; i++) {
@@ -18,6 +25,11 @@ window.onload = function(){
 		if (!(boundaries[0].classList.contains("youlose"))){
 			alert("YOU WIN!");
 		}
+	}
+
+	//exercise 4
+	start.onmouseover = function(){
+		restart();
 	}
 }
 
